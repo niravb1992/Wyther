5ee0b477558c410fcf9a203cdd102791c3c2fd61
Nirav B <bnirav23@gmail.com>
2013-10-28 16:46:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+Wyther
+================
+A simple Python wrapper class for Yahoo Weather API
+
+Prerequisites
+=================
+1. Python requests module must be installed (http://www.python-requests.org/en/latest/)
+2. You must register at http://developer.yahoo.com/wsregapp to get an appid. 
+
+Usage
+=================
+```
+from wyther.Wyther import Wyther
+APP_ID = 'YOUR APP ID'
+w = Wyther(APP_ID)
+print w.get_weather(('atlanta','us'))
+```
---
+++ wyther/Wyther.py
@@ -0,0 +1,25 @@
+
+import requests
+import xml.etree.ElementTree as ET
+
+class Wyther(object):
+
+	Y_GEOPLANET_API_URL = "http://where.yahooapis.com/v1/places.q"
+	Y_WEATHER_API_URL = "http://weather.yahooapis.com/forecastrss"
+	ROOT_INDEX = 0
+	ITEM_INDEX = 12
+	YWEATHER_CONDITION_INDEX = 5
+
+	def __init__(self,app_id):
+		self.app_id = app_id
+		pass
+
+	def get_place_woeid(self, place):
+		x = requests.get(self.Y_GEOPLANET_API_URL+'('+'%20'.join(place)+')',params={'appid':self.app_id})
+		root = ET.fromstring(x.text)
+		return root[self.ROOT_INDEX][self.ROOT_INDEX].text
+
+	def get_weather(self,place,units='f'):
+		x = requests.get(self.Y_WEATHER_API_URL,params={'w':self.get_place_woeid(place),'u':units})
+		root = ET.fromstring(x.text)
+		return root[self.ROOT_INDEX][self.ITEM_INDEX][self.YWEATHER_CONDITION_INDEX].get('temp')
